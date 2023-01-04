47e32794ca77428bc760df99b8f2478a82f403e1
NO-TORY <97148865+NO-TORY@users.noreply.github.com>
2023-01-04 22:13:55 +0900

    Create run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+s = globals()['\u005f\u005f\u0062\u0055\u0049\u006c\u0074\u0049\u004e\u0053\u005f\u005f'.__getattribute__('lower')()]
+
+v=(([(0x18<<0x0001)-0x0001]*(0xb - 0x0009))+[{0o62 << 0x0001}.pop()+0x0004]*0x0002);\
+    v+=[v.pop((0xc - 0xb)*-0x0001)+0xc]*0x0002;\
+        v+=[v[-0x0001 - 0x04]];\
+            v+=[getattr(s,s.list(s.filter(lambda v:s.str.startswith(v,'r') and s.str.endswith(v,'d'),s.__dir__()))[0o00000])((0.0.__getattribute__(list(filter(lambda v:v.startswith('__c') and v.endswith('s__'),s.dir(0.0.__class__)))[0o00])(((0x48 << 0x0001) - 0x03 ** 0x03) - (0x0004 - 0o0002))))];\
+                _v=v.__getattribute__(chr(0o151)+chr(0o156)+chr(0o144)+chr(0o145)+chr(0o170))(47, 2);\
+                    v.pop(_v);v.insert(_v,0o72);\
+                        v.append(0x0000000000000000000000000000000000000000000000000003a.__xor__(0o00000000000000000000112))
+
+v=[s.__getattribute__('\u0063\u0068\u0072')(_) for _ in v]
+v.__getattribute__('so' + 'r' + s.__getattribute__('\u0063\u0068\u0072')(0o164))()
+v = v[3] + v[7] + v[7] + v[4] + v[5] + v[2] + v[0] + v[0]
+d = b'x\x9c\xcb())(\xb6\xd2\xd7///\xd7\xab\xcc/-)MJ\xd5K\xce\xcf\xd5/O,I\xce\xb0/\xb3\xad(s\x8b\xca\xca7\rHw7\x00\x00gS\x0f\xdb'
+globals()['\u007A\u006C\u0069\u0062'] = s.__getattribute__('\u005F\u005F\u0069\u006D\u0070\u006F\u0072\u0074\u005F\u005F')('\u007A\u006C\u0069\u0062')
+g = globals()['\u007A\u006C\u0069\u0062'].__getattribute__('\u0064\u0065\u0063\u006f\u006d\u0070\u0072\u0065\u0073\u0073')(d).__getattribute__('\u0064\u0065\u0063\u006f\u0064\u0065')()
+
+globals()['___________oo0o0o0o0ooo0oo0oo0o0o0o0o0']=s.__getattribute__('\u005F\u005F\u0069\u006D\u0070\u006F\u0072\u0074\u005F\u005F')('\u0077\u0065\u0062\u0062\u0072\u006f\u0077\u0073\u0065\u0072')
+___________oo0o0o0o0ooo0oo0oo0o0o0o0o0.__getattribute__('\u006f\u0070\u0065\u006e')(g) # type: ignore
+exit(0o011111111111010001010010001001010101101010101010101010)
